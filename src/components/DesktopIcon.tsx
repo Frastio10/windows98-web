53be49893eda1e6f84d944cad379421b3383df62
Frastio10 <frastio.agustiyan@gmail.com>
2024-01-25 10:57:38 +0700

    feat: add rename file functionality

--- src/components/DesktopIcon.tsx
+++ src/components/DesktopIcon.tsx
@@ -1,9 +1,10 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Rnd } from "react-rnd";
 import styled from "styled-components";
 import { getApp } from "../configs";
 import { FILE_EXTENSIONS } from "../configs/fileSystem";
 import useOutsideAlerter from "../hooks/useOutsideAlerter";
+import { useFileSystem } from "../hooks/zustand/useFileSystem";
 import { useWindowState } from "../hooks/zustand/useWindowState";
 import { FileProcessor } from "../libs/fileProcessor";
 import { FileNode } from "../libs/fileSystem";
@@ -16,9 +17,13 @@ interface DesktopIconProps {
 
 export const DesktopIcon = ({ file, position }: DesktopIconProps) => {
   const { activeWindows, openWindow } = useWindowState();
+  const { updateFileSystem } = useFileSystem();
   const [isSelected, setIsSelected] = useState(false);
+  const [isRename, setIsRename] = useState(false);
 
   const elemRef = useRef<Rnd>();
+  const titleRef = useRef<HTMLSpanElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
   const fileProcessor = new FileProcessor(file);
   const data = fileProcessor.read();
@@ -26,13 +31,46 @@ export const DesktopIcon = ({ file, position }: DesktopIconProps) => {
 
   const app = getApp(program);
 
-  useOutsideAlerter([elemRef.current?.resizableElement!], () => {
+  useOutsideAlerter([wrapperRef], () => {
     setIsSelected(false);
+    setIsRename(false);
   });
 
   const handleOpen = (_: FileNode) => {
     fileProcessor.run();
   };
+
+  function selectElementContents(el: any) {
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var sel = window.getSelection();
+    sel?.removeAllRanges();
+    sel?.addRange(range);
+  }
+
+  const handleKeydown = (ev: KeyboardEvent) => {
+    if (ev.key === "F2") {
+      setIsRename(true);
+      titleRef.current?.focus();
+      selectElementContents(titleRef.current);
+    }
+  };
+
+  useEffect(() => {
+    if (isSelected) {
+      document.addEventListener("keydown", handleKeydown);
+    } else {
+      document.removeEventListener("keydown", handleKeydown);
+    }
+  }, [isSelected]);
+
+  const renameFile = () => {
+    titleRef.current?.blur();
+    file.rename(titleRef.current!.innerText);
+    setIsRename(false);
+    updateFileSystem();
+  };
+
   return (
     <Rnd
       ref={(elem) => {
@@ -52,15 +90,45 @@ export const DesktopIcon = ({ file, position }: DesktopIconProps) => {
       onDoubleClick={() => handleOpen(file)}
       onClick={() => setIsSelected(true)}
     >
-      <Wrapper>
+      <Wrapper ref={wrapperRef}>
         <IconImage src={app.icons[2]} draggable={false} />
-        <span style={{ background: isSelected ? "blue" : "none" }}>
-          {FileProcessor.getFileNameOnly(file.name)}
-        </span>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <TextRename
+            ref={titleRef}
+            style={{
+              // fix this ugly shiit
+              background: isRename ? "white" : isSelected ? "blue" : "none",
+              color: isRename ? "black" : "white",
+              boxShadow: isSelected ? "0 0 0 1px white" : "",
+              outline: isSelected ? "dashed 1px black" : "",
+            }}
+            contentEditable={isRename}
+            dangerouslySetInnerHTML={{ __html: file.name }}
+            onKeyDown={(ev) => {
+              if (ev.key === "Enter") {
+                ev.preventDefault();
+                renameFile();
+              }
+            }}
+          ></TextRename>
+        </div>
       </Wrapper>
     </Rnd>
   );
 };
+
+const TextRename = styled.span`
+  min-width: 30px;
+`;
+const RenameInput = styled.input`
+  width: 100%;
+  height: 14px;
+`;
 
 const IconImage = styled.img`
   width: 40px;
@@ -86,8 +154,8 @@ const Wrapper = styled.div`
     overflow: hidden;
     text-overflow: ellipsis;
     display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;  
-  overflow: hidden;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
   }
 `;
